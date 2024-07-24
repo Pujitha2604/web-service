458b9bd434ec9206e0fcfa14d2ac1a903c50d8bc
SahilSyed01 <sahilsyed7781@gmail.com>
2024-07-24 09:55:33 +0530

    ps1

--- api_tests.sh
+++ api_tests.sh
@@ -13,10 +13,10 @@ sleep 5
 # Run Newman with Docker, mounting the Docker socket and files
 docker run --network host \
   --name employee-service \
-  -v ./collection/:/etc/postman \
+  -v $(pwd)/collection/:/etc/postman \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -t postman/newman:latest run -r cli,json --reporter-json-export /etc/postman/newman-report.json /etc/postman/collection.json
- 
+
 # Shut down services with Docker Compose
 docker-compose down
  
